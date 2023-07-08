17261a08b0ebe9897d60a7a0b35e870b045b9a7a
suongfiori <suong.kf@gmail.com>
2023-07-08 17:34:50 +0100

    resolve dependencies conflict

--- src/components/VolumeInput.jsx
+++ src/components/VolumeInput.jsx
@@ -3,15 +3,15 @@ import Stack from '@mui/material/Stack';
 import Slider from '@mui/material/Slider';
 import VolumeDown from '@mui/icons-material/VolumeDown';
 import VolumeUp from '@mui/icons-material/VolumeUp';
-import { styled } from '@mui/material/styles';
+// import { styled } from '@mui/material/styles';
 
 
-const CustomSlider = styled(Slider)(({ theme }) => ({
-  color: '#b25244',
-  '& .MuiSlider-thumb': {
-    backgroundColor: '#5e1912',
-  },
-}))
+// const CustomSlider = styled(Slider)(({ theme }) => ({
+//   color: '#b25244',
+//   '& .MuiSlider-thumb': {
+//     backgroundColor: '#5e1912',
+//   },
+// }))
 
 const VolumeInput = ({ volume, onVolumeChange, setMessage, powerState }) => {
   const handleChange = (event, newValue) => {
@@ -23,7 +23,7 @@ const VolumeInput = ({ volume, onVolumeChange, setMessage, powerState }) => {
     <Box sx={{ width: 160 }}>
       <Stack spacing={1} direction="row" sx={{ mb: 1 }} alignItems="center">
         <VolumeDown />
-        <CustomSlider
+        <Slider
           aria-label="Volume"
           value={volume}
           onChange={handleChange}
